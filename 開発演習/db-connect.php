58b8523be32419ed1d6ffb6a2b3389bc7aa456b0
sho0515 <146160851+sho0515@users.noreply.github.com>
2023-10-03 11:00:04 +0900

    Create db-connect.php

---
+++ 開発演習/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER    = 'mysql218.phy.lolipop.lan';
+    const DBNAME    = 'LAA1517459-ensyu';
+    const USER      = 'LAA1517459';
+    const PASS      = 'Pass0515';
+
+    $connect = 'mysql:host='. SERVER .';dbname='. DBNAME .';charset=utf8';
+?>
