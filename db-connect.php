6163b69716a53e15c759a485ad98b2067f57ffd1
sho0515 <146160851+sho0515@users.noreply.github.com>
2023-10-03 10:56:31 +0900

    Add files via upload

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER    = 'mysql215.phy.lolipop.lan';
+    const DBNAME    = 'LAA1517459-shop';
+    const USER      = 'LAA1517459';
+    const PASS      = 'Pass0515';
+
+    $connect = 'mysql:host='. SERVER .';dbname='. DBNAME .';charset=utf8';
+?>
