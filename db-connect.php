03843f2cd2655074f4415b44858af6a2cafd194d
sho0515 <146160851+sho0515@users.noreply.github.com>
2023-10-16 14:34:46 +0900

    Delete db-connect.php

--- db-connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    const SERVER    = 'mysql218.phy.lolipop.lan';
-    const DBNAME    = 'LAA1517459-ensyu';
-    const USER      = 'LAA1517459';
-    const PASS      = 'Pass0515';
-
-    $connect = 'mysql:host='. SERVER .';dbname='. DBNAME .';charset=utf8';
-?>
